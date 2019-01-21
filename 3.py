984cebdac911c03df31153ee58e5bc97170ac4c1
qiaosiyi <qsy2011815@163.com>
2019-01-21 23:53:12 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,3 +1,6 @@
+# coding=UTF-8
+# basic idea:
+#比较任意两个动物的相同特征数目
 def same_num(a,b):
 	summ = 0
 	for i in a:
@@ -6,13 +9,12 @@ def same_num(a,b):
 				summ = summ + 1
 	return summ
 
-
 file_object = open('guess.in', 'rb')
 flag = 1
 for line in file_object:
 	if flag:
 		N = int(line)
-		listtotal = {}
+		listtotal = {}#字典，总体特征中共有元素标记为2，独有元素标记为1，
 		maxnum = 0
 		anm =  [[] for i in range(N)]
 		flag = 0
@@ -23,7 +25,7 @@ for line in file_object:
 		for j in range(cc):
 			anm[i].append(tempinput[j+2])
 			if listtotal.has_key(tempinput[j+2]) == 1: 
-				listtotal.update({tempinput[j+2]:2})
+				listtotal.update({tempinput[j+2]:2})#判断出此元素已经由其他动物建立，那么这个元素就算是共有的，标记：2
 			else:
 				listtotal.update({tempinput[j+2]:1})							
 		i = i + 1
@@ -32,8 +34,8 @@ for i in range(N):
 	for j in range(N):
 		if i != j and i < j:
 			tempsum = same_num(anm[i],anm[j]) + 1
-	if maxnum < tempsum:
-		maxnum = tempsum
+		if maxnum < tempsum:
+			maxnum = tempsum
 
 
 file_object = open('guess.out', 'w')
