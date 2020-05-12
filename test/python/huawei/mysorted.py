0cfa6e8907a29a09fe47335abbf10a610eb2d12c
qiaosiyi <qsy2011815@163.com>
2020-05-12 17:37:55 +0800

    add

--- test/python/huawei/mysorted.py
+++ test/python/huawei/mysorted.py
@@ -6,7 +6,32 @@ class Student:
     self.grade = grade
     self.age = age
   def __repr__(self):
-    return repr((self.name, self.grade, self.age))
+    return "%s %d %d" % (self.name, self.grade, self.age)
+
+def my_cmp(self, other):
+    return self.grade - other.grade
+    
+def my_cmp2(self,other):
+    if self.grade > other.grade:
+      return 1
+    elif self.grade == other.grade:
+      if self.age < other.age:
+        return 1
+      else:
+        return -1
+    else:
+      return -1
+
+student_objects = [
+    Student('john', 92, 15),
+    Student('jane', 94, 12),
+    Student('dave', 94, 10)
+    ]
+
+print sorted(student_objects,cmp=my_cmp2,reverse=True)
+
+
+
 
 
 
