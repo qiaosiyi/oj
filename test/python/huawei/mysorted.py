903d6f257777df70f853df4d698eb4e49c4cc5cb
qiaosiyi <qsy2011815@163.com>
2020-05-12 10:13:46 +0800

    add

--- test/python/huawei/mysorted.py
+++ test/python/huawei/mysorted.py
@@ -1,2 +1,16 @@
 # coding=UTF-8
 # 学习使用sorted函数，传入一个比
+class Student:
+  def __init__(self, name, grade, age):
+    self.name = name
+    self.grade = grade
+    self.age = age
+  def __repr__(self):
+    return repr((self.name, self.grade, self.age))
+
+
+
+
+
+
+    
